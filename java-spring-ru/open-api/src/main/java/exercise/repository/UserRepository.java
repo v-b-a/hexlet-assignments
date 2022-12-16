95c39713846887a7186779fbdcb6a1c8034d296d
@hexlet/cli <support@hexlet.io>
2022-12-16 08:15:02 +0300

    submit java-spring-ru/open-api

---
+++ java-spring-ru/open-api/src/main/java/exercise/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package exercise.repository;
+
+import exercise.model.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+}
