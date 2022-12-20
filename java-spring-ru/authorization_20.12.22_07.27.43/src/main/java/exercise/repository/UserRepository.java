aa8e3afc2313db6fe6bbd42f8d810c42c9694b58
@hexlet/cli <support@hexlet.io>
2022-12-20 07:30:52 +0300

    submit java-spring-ru/authorization_20.12.22_07.27.43

---
+++ java-spring-ru/authorization_20.12.22_07.27.43/src/main/java/exercise/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package exercise.repository;
+
+//import exercise.model.User;
+import exercise.model.User;
+import org.springframework.data.repository.CrudRepository;
+//import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    // BEGIN
+    Optional<User> findByEmail(String email);
+    // END
+}
